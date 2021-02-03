9a792a06217bf1e8d06d8636304e86f0b0214dc2
everywherewego <964353527@qq.com>
2021-02-03 20:52:33 +0800

    Jenkins 测试

--- src/main/java/com/bfd/Main.java
+++ src/main/java/com/bfd/Main.java
@@ -1,15 +1,5 @@
 package com.bfd;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.bfd.control.ControlApi.insertsinglepic;
-import static com.bfd.utils.HttpClientHelper.sendGetToGetPicture;
-import static com.bfd.utils.f_SqlUtil.querySql;
-
 /**
  * @author everywherewego
  * @date 1/25/21 9:44 AM
@@ -17,34 +7,35 @@ import static com.bfd.utils.f_SqlUtil.querySql;
 
 public class Main {
     public static void main(String[] args) {
-        int pageSize = 100;
-        Map<String, String> requestHeaders = new HashMap<>();
-        requestHeaders.put("Accept", "*/*");
-        requestHeaders.put("accept-encoding", "gzip, deflate");
-
-        JSONObject param = new JSONObject();
-        JSONArray params = new JSONArray();
-        for (int i = 0; ; i++) {
-            JSONObject jsonObject = new JSONObject();
-
-            JSONArray jsonArray = querySql("select object_name, object_business_code from object_info order by id limit " + i * pageSize + " , " + pageSize);
-            for (int j = 0; j < jsonArray.size(); j++) {
-                String name = jsonArray.getJSONObject(j).getString("object_name");
-                String idcard = jsonArray.getJSONObject(j).getString("object_business_code");
-                String base64String = sendGetToGetPicture("http://10.236.54.235:13734/img?SFZH=" + idcard, requestHeaders);
-                if (null == base64String) {
-                    continue;
-                }
-                jsonObject.put("repository_id", "1");
-                jsonObject.put("picture_image_content_base64", base64String);
-                jsonObject.put("person_id", idcard);
-                params.add(jsonObject);
-            }
-            param.put("images", params);
-            insertsinglepic(param);
-            if (jsonArray.size() < pageSize) {
-                break;
-            }
-        }
+//        int pageSize = 100;
+//        Map<String, String> requestHeaders = new HashMap<>();
+//        requestHeaders.put("Accept", "*/*");
+//        requestHeaders.put("accept-encoding", "gzip, deflate");
+//
+//        JSONObject param = new JSONObject();
+//        JSONArray params = new JSONArray();
+//        for (int i = 0; ; i++) {
+//            JSONObject jsonObject = new JSONObject();
+//
+//            JSONArray jsonArray = querySql("select object_name, object_business_code from object_info order by id limit " + i * pageSize + " , " + pageSize);
+//            for (int j = 0; j < jsonArray.size(); j++) {
+//                String name = jsonArray.getJSONObject(j).getString("object_name");
+//                String idcard = jsonArray.getJSONObject(j).getString("object_business_code");
+//                String base64String = sendGetToGetPicture("http://10.236.54.235:13734/img?SFZH=" + idcard, requestHeaders);
+//                if (null == base64String) {
+//                    continue;
+//                }
+//                jsonObject.put("repository_id", "1");
+//                jsonObject.put("picture_image_content_base64", base64String);
+//                jsonObject.put("person_id", idcard);
+//                params.add(jsonObject);
+//            }
+//            param.put("images", params);
+//            insertsinglepic(param);
+//            if (jsonArray.size() < pageSize) {
+//                break;
+//            }
+//        }
+        System.out.println("jenkisc测试");
     }
 }
